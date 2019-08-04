baa3896ea4f35aa4b20951967d85ef860653d133
Viktor Tomilin <viktortomilin@gmail.com>
2019-08-04 21:47:44 +0300

    Fix a syntax issues by standard

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var lifoRouter = require('./routes/api/v1/lifo');
+var indexRouter = require('./routes/index')
+var lifoRouter = require('./routes/api/v1/lifo')
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/api/v1/lifo', lifoRouter);
+app.use('/', indexRouter)
+app.use('/api/v1/lifo', lifoRouter)
 
-module.exports = app;
+module.exports = app
--- routes/api/v1/lifo.js
+++ routes/api/v1/lifo.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(null);
-});
+router.get('/', function (req, res, next) {
+  res.send(null)
+})
 
-router.post('/', function(req, res, next) {
-  res.send(null);
-});
+router.post('/', function (req, res, next) {
+  res.send(null)
+})
 
-module.exports = router;
+module.exports = router
