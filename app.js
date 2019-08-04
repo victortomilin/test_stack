84fc661f683a35437cedac909810cb612c7f6f2b
Viktor Tomilin <viktortomilin@gmail.com>
2019-08-04 22:30:11 +0300

    Use LIFO middleware

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-var indexRouter = require('./routes/index')
-var lifoRouter = require('./routes/api/v1/lifo')
+const lifoMiddleware = require('./middlewares/lifo')
 
-var app = express()
+const indexRouter = require('./routes/index')
+const lifoRouter = require('./routes/api/v1/lifo')
+
+const app = express()
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -15,6 +17,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/api/v1/lifo', lifoRouter)
+app.use('/api/v1/lifo', lifoMiddleware(), lifoRouter)
 
 module.exports = app
--- routes/api/v1/lifo.js
+++ routes/api/v1/lifo.js
@@ -2,11 +2,12 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.send(null)
+  res.send({ word: req.lifo.get() })
 })
 
 router.post('/', function (req, res, next) {
-  res.send(null)
+  req.lifo.add(req.body.word)
+  res.end()
 })
 
 module.exports = router
