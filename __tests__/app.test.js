22c12973f0df96d57876f67bb965cee8bb313270
Viktor Tomilin <viktortomilin@gmail.com>
2019-08-04 22:38:25 +0300

    Add unit tests to cover key-value storage router

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -23,5 +23,37 @@ describe('Test api', () => {
           .expect(200, { word: 'foo' }, done)
       })
     })
+
+    describe('store', () => {
+      describe('no TTL', () => {
+        test('POST method', (done) => {
+          request(app)
+            .post('/api/v1/store')
+            .type('json')
+            .send([
+              { key: 'name', value: 'John' }
+            ])
+            .set('Accept', /application\/json/)
+            .then(response => {
+              expect(response.statusCode).toBe(200)
+              done()
+            })
+        })
+
+        test('GET method', (done) => {
+          request(app)
+            .get('/api/v1/store/name')
+            .set('Accept', /application\/json/)
+            .expect(200, 'John', done)
+        })
+
+        test('DELETE method', (done) => {
+          request(app)
+            .delete('/api/v1/store/name')
+            .set('Accept', /application\/json/)
+            .expect(200, done)
+        })
+      })
+    })
   })
 })
