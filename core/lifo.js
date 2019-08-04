774b8af47df452263c5b68fd48f9fd2bf7e189a8
Viktor Tomilin <viktortomilin@gmail.com>
2019-08-04 22:07:37 +0300

    Add a LIFO module implementation

--- core/lifo.js
+++ core/lifo.js
@@ -1,5 +1,10 @@
 function lifo () {
+  const stack = []
 
+  const add = value => stack.push(value)
+  const get = () => stack.pop()
+
+  return Object.freeze({ add, get })
 }
 
 module.exports = lifo
